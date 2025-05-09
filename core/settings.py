a440c465b6c48c764c33b6cb9973870e83f49d95
rex9840 <rupeshrnepal@gmail.com>
2025-05-09 23:13:19 +0545

    feat: csrf trusted origin

--- core/settings.py
+++ core/settings.py
@@ -97,6 +97,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "core.wsgi.application"
 
+CSRF_TRUSTED_ORIGINS = ["https://" +SERVER_HOST] 
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
